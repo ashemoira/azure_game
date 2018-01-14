cd17a0af3192cec49e01018be616d6ee8abad99a
Magnolis_Mint <ashe.moira@gmail.com>
2018-01-14 23:00:54 +0900

    add unit model

---
+++ spec/factories/units.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :unit do
+    name "MyString"
+    type 1
+    vanguard_left "MyString"
+    vanguard_center "MyString"
+    vanguard_right "MyString"
+    mainstay_left "MyString"
+    mainstay_center "MyString"
+    mainstay_right "MyString"
+    comment "MyString"
+  end
+end
