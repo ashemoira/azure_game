b4b7ac7051c545cfc5d63a2676666cf36f5eb3d6
Magnolis_Mint <ashe.moira@gmail.com>
2018-01-22 01:03:00 +0900

    add ship controller

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,54 @@
+class ShipsController < ApplicationController
+  before_action :fetch_ship, only: %i[show edit update]
+  before_action :fetch_all_ship, only: %i[index]
+
+  permits :name,
+          :camp,
+          :rarity,
+          :ship_type,
+          :introduction,
+          :fleet_position
+
+  def new
+    @ship = Ship.new
+  end
+
+  def index;end
+
+  def show;end
+
+  def edit;end
+
+  def create(ship)
+    @ship = Ship.new(ship)
+    if @ship.invalid?
+      @ship.save
+      redirect_to @ship
+    else
+      redirect_back(fallback_location: ships_new_path)
+    end
+  end
+
+  def update
+    if @ship.update(unit)
+      redirect_to @ship
+    else
+      redirect_back(fallback_location: ships_edit_path)
+    end
+  end
+
+  def destroy
+    @ship.destroy
+    redirect_to ship_index_path
+  end
+
+    private
+
+    def fetch_ship(id)
+      @ship = Ship.find(id)
+    end
+
+    def fetch_all_ship
+      @ships = Ship.all
+    end
+end
