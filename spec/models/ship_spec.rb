69bd1efeaeb60adbecb5c6c285ba4c2955ae2cbb
Magnolis_Mint <ashe.moira@gmail.com>
2018-01-25 01:09:06 +0900

    add validate test to ship name

--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Ship, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validates' do
+    let(:ship) { Ship.new(id: 99999, camp: 1, rarity: 1, ship_type: 1, fleet_position: 1) }
+    it 'should be name validate' do
+      expect(ship).not_to be_valid
+    end
+  end
 end
